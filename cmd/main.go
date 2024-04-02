a5500caf9efde345c10fc8fd9555e03c3abc76de
OrmayG <113803159+OrmayG@users.noreply.github.com>
2024-04-02 11:41:37 +0200

    working webserver

--- cmd/main.go
+++ cmd/main.go
@@ -16,14 +16,26 @@ func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Co
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
-func newtemplate() *Templates {
+func newTemplate() *Templates {
 	return &Templates{
 		templates: template.Must(template.ParseGlob("views/*.html")),
 	}
 }
 
+type Count struct {
+	Count int
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
+	count := Count{Count: 0}
+	e.Renderer = newTemplate()
 
+	e.GET("/", func(c echo.Context) error {
+		count.Count++
+		return c.Render(200, "index", count)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
